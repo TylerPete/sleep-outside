6e4037ba5a1d47f7bf086d7e17b7b0cae674f10d
pdmoura <sudwarriormoura@gmail.com>
2025-11-06 02:50:55 -0300

    feat: Implemented product details page and cart functionality; updated product links and added necessary scripts

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -33,3 +33,19 @@ export function updateCartCount() {
   // If 0, it will be an empty string, and the CSS will hide it.
   cartCountElement.textContent = cartItems.length > 0 ? cartItems.length : "";
 }
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
