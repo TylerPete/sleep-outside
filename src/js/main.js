821ef68ac8b3730f055aa6d3bbb1b714cf461807
eriko13 <erikgerman917@gmail.com>
2025-11-12 09:03:39 -0700

    Created Alert.js and added it in main.js
    - Added Alert functionality in main.js to initialize alerts on page load.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 import { updateCartCount } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.js";
 
 // Run the function on page load
 updateCartCount();
+
+// Initialize alerts
+const alert = new Alert();
+alert.init();
 
 const productData = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
