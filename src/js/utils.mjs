6ae7d932ad64a7e115cacf0799c79066262f207e
pdmoura <sudwarriormoura@gmail.com>
2025-11-15 17:25:28 -0300

    refactor: clean up loadHeaderFooter function

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -67,18 +67,17 @@ export function renderWithTemplate(template, parentElement, position = "afterbeg
 
 export async function loadTemplate(path) {
   const response = await fetch(path);
-  console.log(response);
   const template = await response.text();
   return template;
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate('../partials/header.html');
-  const footerTemplate = await loadTemplate('../partials/footer.html');
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
 
 
-  const headerElement = document.querySelector('#main-header');
-  const footerElement = document.querySelector('#main-footer');
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
