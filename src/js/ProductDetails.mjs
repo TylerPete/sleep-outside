3b129ce9e8c62369fb59bd862e1e33930c5f5a05
pdmoura <sudwarriormoura@gmail.com>
2025-11-23 03:00:33 -0300

    feat: enhance checkout and product details with improved styling and alert messages

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, updateCartCount, qs, loadHeaderFooter } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, updateCartCount, qs, loadHeaderFooter, alertMessage } from "./utils.mjs";
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -51,6 +51,7 @@ export default class ProductDetails {
         cartIcon.classList.remove("cart-shake");
       }, 400);
     }
+    alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
   }
 
   /**
